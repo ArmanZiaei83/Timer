1a01ca9207c0da06266704ed8795d7eac71fc478
ArmanZiaei <armanziaei83@gmail.com>
2020-10-31 00:32:03 +0330

    Feats Xml & Java File Codes : these files added .

---
+++ app/src/main/java/com/example/chornometer/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.chornometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Chronometer chronometer ;
+    private long pauseOffSet ;
+    boolean runnnig ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chrnometer_0);
+
+    }
+    public void  startChronometer (View v) {
+        if (!runnnig){
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffSet);
+            chronometer.start();
+            runnnig = true ;
+        }
+    }
+
+    public void  pauseChronometer (View v) {
+        if (runnnig){
+            chronometer.stop();
+            pauseOffSet = SystemClock.elapsedRealtime() - chronometer.getBase() ;
+            runnnig = false ;
+        }
+    }
+
+    public void  resetChronometer (View v) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        runnnig = true; ;
+    }
+}
